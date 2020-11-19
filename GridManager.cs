6ffe336c023ac669dbe1912664601b58bdcb3269
hhphngchi <51274605+hhphngchi@users.noreply.github.com>
2020-11-19 11:25:15 -0800

    Add files via upload

---
+++ GridManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int num_diffs = 5;
+	public int num_total = 10;
+
+	// grid width and height
+	public int width, height;
+
+	// placemat
+	public GameObject Tile;
+	public int tileWidth, tileHeight;
+	List<GameObject> gridPositions;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject[] gridPositions = new GameObject[num_total];
+		gridPositions = new List<GameObject>();
+		 // ganerate the grid
+		for (int x = 0; x < width; x += tileWidth)
+		{
+			for (int y = 0; y < height; y += tileHeight)
+			{
+				Vector3 originPosition = new Vector3(this.transform.position.x + x/2, this.transform.position.y + y/2, this.transform.position.z);
+				GameObject pos = Instantiate(Tile, originPosition, Quaternion.identity);
+
+				// adjust position's size
+				x = tileWidth;
+				y = tileHeight;
+				gridPositions.Add(pos);
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
